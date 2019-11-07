0fa8511945f086426ac4301aec488cba33cf886f
Javier Acevedo <javieracevedo1987@gmail.com>
2019-11-07 10:37:20 +0100

    core: composization of todos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { FooterComponent } from "./footer/footer.component";
+import { TodoComponent } from "./todo/todo.component";
+import { TodoFooterComponent } from "./todo/todo-footer/todo-footer.component";
+import { TodoAddComponent } from "./todo/todo-add/todo-add.component";
+import { TodoItemComponent } from "./todo/todo-item/todo-item.component";
+import { TodoListComponent } from "./todo/todo-list/todo-list.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    TodoComponent,
+    TodoFooterComponent,
+    TodoAddComponent,
+    TodoItemComponent,
+    TodoListComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
